32e82e9e939435fd3e5b6ddf36488d16a65928f1
liliaungurean <lilicikau95@gmail.com>
2022-11-25 00:56:11 -0600

    html/JS update

---
+++ Develop/script.js
@@ -0,0 +1,17 @@
+//variables
+//console.log(dayjs().format());
+
+//variable for current hour
+
+//Set Data attributes to each hour
+
+//jQuery
+
+//Change rou color to reflet current hour
+
+// save to local storage
+//saveBtn.on("click" function(){
+
+//})
+
+//retrived stored input
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Calendar
+
+https://www.w3schools.com/bootstrap4/bootstrap_grid_examples.asp
+https://www.w3schools.com/icons/tryit.asp?filename=tryicons_fa-save
