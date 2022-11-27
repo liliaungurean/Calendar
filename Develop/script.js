af75ec301354b41626f19280d89e78cc34cd6816
liliaungurean <lilicikau95@gmail.com>
2022-11-26 23:06:35 -0600

    colors to reflet current hour, past and future

--- Develop/script.js
+++ Develop/script.js
@@ -6,24 +6,37 @@ var currentDay = $("#currentDay");
 //display current date/time
 $(function(){
     var today = dayjs().format("MMMM D, YYYY, h:mm");
-    //console.log(today);
+    console.log(today);
     currentDay.append(today);
 
 //console.log(currnetDay);
-
-//jQuery
 
 //Change row color to reflet current hour, past and future
 function changeColor(){
 
     var now = dayjs().hour();
-    console.log("It is" + now + "hour");
+    console.log("It is " + now + " hour");
 
     $(".time-block").each(function() {
-        
+        var calTime = $(this).attr("id").split(".hour");
+
+        if (now > calTime) {
+           $(this).removeClass("present");
+           $(this).removeClass("future");
+           $(this).addClass("past");
+        } else if (now < calTime){
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }else if (now !== calTime){
+           $(this).removeClass("past");
+           $(this).removeClass("future");
+           $(this).addClass("present");
+        }
+
     })
 }
-
+ changeColor();
 // save to local storage
 $(".saveBtn").on("click", function(){
     var hour = $(this).parent().attr("id");
@@ -33,5 +46,6 @@ $(".saveBtn").on("click", function(){
 })
     
     //retrived stored input
+    $("#hour-7 .schedule") .val(localStorage.getItem("hour-7"));
 })
 
--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 https://www.w3schools.com/bootstrap4/bootstrap_grid_examples.asp
 https://www.w3schools.com/icons/tryit.asp?filename=tryicons_fa-save
 https://api.jquery.com/each/
+https://bobbyhadz.com/blog/javascript-remove-class-if-exists#:~:text=To%20remove%20a%20class%20if,'bg%2Dyellow')%20.
+
+https://api.jquery.com/addclass/
