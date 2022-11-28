e97b33b8557d332a724d4bf585ebcebbca574425
liliaungurean <lilicikau95@gmail.com>
2022-11-27 22:01:07 -0600

    local storage JS and ReadMe

--- Develop/script.js
+++ Develop/script.js
@@ -1,51 +1,91 @@
 //variables
 var saveBtn = $(".saveBtn");
-var timeBlockEl = $(".time-block");
-var currentDay = $("#currentDay");
+var clearBtn = $(".clearBtn");
+var scheduleEl= $(".schedule");
+
+
 
 //display current date/time
-$(function(){
-    var today = dayjs().format("MMMM D, YYYY, h:mm");
-    console.log(today);
-    currentDay.append(today);
+    $(function(){
+        var today = dayjs().format("MMMM D, YYYY, h:mm");
+        console.log(today);
+        currentDay.append(today);
 
-//console.log(currnetDay);
 
-//Change row color to reflet current hour, past and future
-function changeColor(){
+//Change row colors to reflet current hour, past and future
+    function changeColor(){
 
-    var now = dayjs().hour();
-    console.log("It is " + now + " hour");
+        var now = dayjs().hour();
+        console.log("It is " + now + " hour");
 
     $(".time-block").each(function() {
-        var calTime = $(this).attr("id").split(".hour");
-
-        if (now > calTime) {
-           $(this).removeClass("present");
-           $(this).removeClass("future");
-           $(this).addClass("past");
-        } else if (now < calTime){
+        var timeBlock = parseInt($(this).attr("id").split(".hour"));
+        
+        if (timeBlock < now) {
+            $(this).addClass("past");
+          }
+          else if (timeBlock === now) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+          }
+          else {
             $(this).removeClass("past");
             $(this).removeClass("present");
             $(this).addClass("future");
-        }else if (now !== calTime){
-           $(this).removeClass("past");
-           $(this).removeClass("future");
-           $(this).addClass("present");
-        }
+          }
+
+    //    if (now > calTime) {
+      //     $(this).removeClass("present");
+     //      $(this).removeClass("future");
+     //      $(this).addClass("past");
+     //   } 
+     //   else if (now === calTime){
+     //       $(this).removeClass("past");
+     //       $(this).removeClass("future");
+     //       $(this).addClass("present");
+     //    }
+     //    else if (now < calTime){
+     //       $(this).removeClass("past");
+     //       $(this).removeClass("present");
+     //       $(this).addClass("future");
+      //  }
 
     })
 }
- changeColor();
-// save to local storage
-$(".saveBtn").on("click", function(){
-    var hour = $(this).parent().attr("id");
-    var textarea = $(this). sibling(".schecule").val();
+//call the function to change color
+    changeColor();
 
-    localStorage.setItem(hour, textarea);
-})
+//clear btn
+clearBtn.on('click', function () {
+    scheduleEl.empty();
+  });
+
+// save button
+    $(saveBtn).on("click", function(){
+
+        var textarea = $(this).siblings(".schecule").val();
+        var hour = $(this).parent().attr("id");
+
+
+//save to local storage
+        localStorage.setItem(textarea, hour);
+    })
     
-    //retrived stored input
+//retrived stored input
     $("#hour-7 .schedule") .val(localStorage.getItem("hour-7"));
-})
+    $("#hour-8 .schedule") .val(localStorage.getItem("hour-8"));
+    $("#hour-9 .schedule") .val(localStorage.getItem("hour-9"));
+    $("#hour-10 .schedule") .val(localStorage.getItem("hour-10"));
+    $("#hour-11 .schedule") .val(localStorage.getItem("hour-11"));
+    $("#hour-12 .schedule") .val(localStorage.getItem("hour-12"));
+    $("#hour-13 .schedule") .val(localStorage.getItem("hour-13"));
+    $("#hour-14 .schedule") .val(localStorage.getItem("hour-14"));
+    $("#hour-15 .schedule") .val(localStorage.getItem("hour-15"));
+    $("#hour-16 .schedule") .val(localStorage.getItem("hour-16"));
+    $("#hour-17 .schedule") .val(localStorage.getItem("hour-17"));
+    $("#hour-20 .schedule") .val(localStorage.getItem("hour-20"));
+    $("#hour-21 .schedule") .val(localStorage.getItem("hour-21"));
+    
+
+});
 
--- README.md
+++ README.md
@@ -1,8 +1,33 @@
 # Calendar
+#Description
+To create a page where the user is able to generate a random password. Each password is chosen at random and none of them are the same. The  page allows the user to select the length of the password and if the user wants capital, lower, numbers or special characters, then generates a random password with the users selected choices.
+
+#Table of Contents
+Installation
+Usage
+Credits
+License
+
+#Installation
+There are no steps required to install this. The users open the webpage on A web browser application. 
+#Usage
+
+#Credits
 
 https://www.w3schools.com/bootstrap4/bootstrap_grid_examples.asp
 https://www.w3schools.com/icons/tryit.asp?filename=tryicons_fa-save
 https://api.jquery.com/each/
 https://bobbyhadz.com/blog/javascript-remove-class-if-exists#:~:text=To%20remove%20a%20class%20if,'bg%2Dyellow')%20.
-
+https://stackoverflow.com/questions/12481439/jquery-this-keyword
 https://api.jquery.com/addclass/
+
+#License
+MIT License
+
+Copyright (c) 2022 liliaungurean
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
